2a1dcf9a263d0454505a91182b8d4b9b4382cce6
Manuel Barajas <mbarajas@syn-2600-6c5a-467f-8299-a166-ab60-97c8-4d30.biz6.spectrum.com>
2025-02-18 09:26:48 -0500

    commenting out color thing from snacks

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -5,40 +5,40 @@ return {
   ---@type snacks.Config
   opts = {
     bigfile = { enabled = true },
-    dashboard = { 
+    dashboard = {
       enabled = true,
       sections = {
-        { section = "header" },
+        { section = 'header' },
         {
           pane = 2,
-          section = "terminal",
-          cmd = "colorscript -e square",
+          section = 'terminal',
+          --cmd = "colorscript -e square",
           height = 5,
           padding = 1,
         },
-        { section = "keys", gap = 1, padding = 1 },
-        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-        { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = 'keys', gap = 1, padding = 1 },
+        { pane = 2, icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+        { pane = 2, icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
         {
           pane = 2,
-          icon = " ",
-          title = "Git Status",
-          section = "terminal",
+          icon = ' ',
+          title = 'Git Status',
+          section = 'terminal',
           enabled = function()
             return Snacks.git.get_root() ~= nil
           end,
-          cmd = "git status --short --branch --renames",
+          cmd = 'git status --short --branch --renames',
           height = 5,
           padding = 1,
           ttl = 5 * 60,
           indent = 3,
         },
-        { section = "startup" },
+        { section = 'startup' },
       },
     },
     explorer = { enabled = true, replace_netrw = true },
     indent = { enabled = true },
-    input = { enabled = true},
+    input = { enabled = true },
     picker = { enabled = true },
     notifier = { enabled = true },
     notify = { enabled = true },
